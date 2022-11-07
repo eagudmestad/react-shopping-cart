754426734a1e5f3fc9e9d870d3d4fae33d94b505
eagudmestad <evangudmestad@gmail.com>
2022-11-07 15:14:00 -0600

    Finished Part 3

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,3 +1,5 @@
+import './CartItem.css';
+
 function CartItem({ item, onNameChange, onQuantityAdd, onQuantityRemove }) {
   return (
     <div className="CartItem d-flex" id={`item-${item.id}`}>
@@ -6,7 +8,8 @@ function CartItem({ item, onNameChange, onQuantityAdd, onQuantityRemove }) {
         id={`item-${item.id}-name`}
         type="text"
         value={item.name}
-        className="form-control CartItem-name"
+        //className={"form-control CartItem-name " + (item.name ? 'is-valid':'is-invalid')}
+        className={`form-control CartItem-name ${item.name ?'is-valid':'is-invalid'}`}
         onChange={(evt) => onNameChange(evt)}
       />
       <label htmlFor={`item-${item.id}-name`}className='visually-hidden'>Quantity</label>
@@ -18,7 +21,7 @@ function CartItem({ item, onNameChange, onQuantityAdd, onQuantityRemove }) {
         className="btn btn-primary m-1 rounded-circle CartItem-add"
         onClick={(evt) => onQuantityAdd(evt)}
         id='btnAdd'
-        disabled={item.quantity===10}
+        disabled={item.quantity>=10}
       >
         <span>+</span>
         <span className="visually-hidden">Add</span>
@@ -27,7 +30,7 @@ function CartItem({ item, onNameChange, onQuantityAdd, onQuantityRemove }) {
         type="button"
         className="btn btn-danger m-1 rounded-circle CartItem-remove"
         onClick={(evt) => onQuantityRemove(evt)}
-        disabled={item.quantity===0}
+        disabled={item.quantity <= 0}
       >
         <span>-</span>
         <span className="visually-hidden">Remove</span>
--- src/CartList.js
+++ src/CartList.js
@@ -1,12 +1,13 @@
 import CartItem from './CartItem';
 import { useState } from 'react';
 import _ from 'lodash';
+import {nanoid} from 'nanoid';
 
 function CartList() {
   const [items, setItems] = useState([
-    { id: 1, name: 'Hat', quantity: 2 },
-    { id: 2, name: 'Tie', quantity: 2 },
-    { id: 3, name: 'Belt', quantity: 1 },
+    { id: nanoid(), name: 'Hat', quantity: 2 },
+    { id: nanoid(), name: 'Tie', quantity: 2 },
+    { id: nanoid(), name: 'Belt', quantity: 1 },
   ]);
 
   function onNameChange(evt, item) {
@@ -17,39 +18,69 @@ function CartList() {
   }
 
   function onQuantityAdd(evt, item) {
-    if (item.quantity >= 0 && item.quantity < 10) {
-      setItems(
-        _.map(items, (x) =>
-          x.id === item.id ? { ...x, quantity: ++x.quantity } : x
-        )
-      );
-    }else{
-      
-    }
+    setItems(
+      _.map(items, (x) =>
+        x.id === item.id ? { ...x, quantity: ++x.quantity } : x
+      )
+    );
   }
 
   function onQuantityRemove(evt, item) {
-    if (item.quantity > 0 && item.quantity <= 10) {
+    const newQuantity = item.quantity - 1;
+    if (newQuantity > 0) {
       setItems(
         _.map(items, (x) =>
           x.id === item.id ? { ...x, quantity: --x.quantity } : x
         )
       );
+    } else {
+      setItems(_.filter(items, (x) => x.id !== item.id));
+    }
+  }
+
+  let itemCount = 0;
+  for (const item of items) {
+    if (item && item.quantity) {
+      itemCount += item.quantity;
     }
   }
 
   return (
     <div className="CartList">
-      <h1>Shopping Cart</h1>
-      {_.map(items, (item) => (
-        <CartItem
-          key={item.id}
-          item={item}
-          onNameChange={(evt) => onNameChange(evt, item)}
-          onQuantityAdd={(evt) => onQuantityAdd(evt, item)}
-          onQuantityRemove={(evt) => onQuantityRemove(evt, item)}
-        />
-      ))}
+      <h1 className="CartList-header d-flex align-items-start my-3">
+        <span>Shopping Cart</span>
+        {itemCount > 0 && (
+          <span className="badge bg-primary ms-2 rounded-pill fs-4">
+            {itemCount}
+          </span>
+        )}
+      </h1>
+      <div className='my-3'>
+        <button
+          type="click"
+          className="CartList-add btn btn-primary m-1"
+          onClick={(evt) =>
+            setItems([
+              ...items,
+              { id: nanoid(), name: '', quantity: 1 },
+            ])
+          }
+        >
+          Add Item
+        </button>
+      </div>
+      <div className='my-3'>
+        {itemCount <=0 && <div className='CartList-empty fst-italic fs-3 text-danger'>Your cart is empty! Add some items to it.</div>}
+        {_.map(items, (item) => (
+          <CartItem
+            key={item.id}
+            item={item}
+            onNameChange={(evt) => onNameChange(evt, item)}
+            onQuantityAdd={(evt) => onQuantityAdd(evt, item)}
+            onQuantityRemove={(evt) => onQuantityRemove(evt, item)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
